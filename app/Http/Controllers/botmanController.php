1b6700ac5570f16134dc456fbcf866122d9f12c1
Nency <djsoutsourcing.dev@gmail.com>
2024-05-01 18:47:52 +0530

    chatbot created msg also show like hi

---
+++ app/Http/Controllers/botmanController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\BotMan\Messages\Incoming\Answer;
+
+class botmanController extends Controller
+{
+    public function callback($botman)
+    {
+        //$botman = app('botman');
+        $botman->fallback(function($bot) {
+            $bot->reply('Sorry, I did not understand these commands. Here is a list of commands I understand: ...');
+        }); 
+    
+    }
+
+    public function handle()
+    {
+        
+        $botman = app('botman');
+        // $botman->hears('My First Message', function ($bot) {
+        //     $bot->reply('Your First Response');
+        // });
+        $botman->hears('{message}', function($botman, $message) {
+  
+            if ($message == 'hi') {
+                $this->askName($botman);
+            }
+            else{
+               // $botman->reply($callback);
+                //this is not working 
+                return $botman->callback();
+            }
+  
+        });
+  
+        $botman->listen();
+    }
+  
+    /**
+     * Place your BotMan logic here.
+     */
+    public function askName($botman)
+    {
+        $botman->ask('Hello! What is your Name?', function(Answer $answer) {
+  
+            $name = $answer->getText();
+  
+            $this->say('Nice to meet you '.$name);
+        });
+    }
+
+
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+       
+
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\botmanController;
+use Illuminate\Support\Facades\Route;
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Middleware\Dialogflow;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('botman');
+    });
+  
+   Route::controller(BotManController::class)->group(function () {
+    Route::match(['get', 'post'], '/botman','handle');
+});
+ 
